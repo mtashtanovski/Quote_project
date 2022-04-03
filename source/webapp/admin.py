3444d464ec5ba23325e826f6eb2b08cf8c488acf
mtashtanovski <tashtanov.m@mail.ru>
2022-04-03 20:19:44 +0600

    1. Created model of Quote, made migrations and applied them. 2. Registered new model in admin panel.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Quote
+
+
+class QuoteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'text', 'author', 'email', 'rating', 'is_moderated', 'created_at']
+    search_fields = ['text', 'author']
+    fields = ['id', 'text', 'author', 'email', 'is_moderated', 'created_at']
+    readonly_fields = ['id', 'created_at']
+
+
+admin.site.register(Quote, QuoteAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+
 # Create your models here.
+class Quote(models.Model):
+    text = models.TextField(
+        max_length=200,
+        null=False,
+        blank=False,
+        verbose_name="Текст"
+    )
+    author = models.CharField(
+        max_length=20,
+        null=False,
+        blank=False,
+        verbose_name="Автор"
+    )
+    email = models.EmailField(
+        verbose_name="Почта"
+    )
+    rating = models.IntegerField(
+        default=0,
+        verbose_name="Рейтинг"
+    )
+    is_moderated = models.BooleanField(
+        default=False,
+        verbose_name="Статус"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания"
+    )
